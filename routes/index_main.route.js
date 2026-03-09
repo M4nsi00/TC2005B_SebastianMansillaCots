d95473111dc5aaeb2f208e1deb2e4d0fad7fdd0e
M4nsi00 <sebastian.mansillacots@gmail.com>
2026-03-09 16:53:28 -0600

    Avance lab18 con Charly

--- routes/index_main.route.js
+++ routes/index_main.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../util/is-auth');
 
 const indexController = require('../controllers/index_main.controller');
 
-router.get('/', indexController.get_index);
-router.get('/referencias', indexController.get_referencias);
-router.get('/labs', indexController.get_labs);
+router.get('/', isAuth, indexController.get_index);
+router.get('/referencias', isAuth, indexController.get_referencias);
+router.get('/labs', isAuth, indexController.get_labs);
 
 module.exports = router;
--- routes/labs.route.js
+++ routes/labs.route.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../util/is-auth');
 
 const labsController = require('../controllers/labs.controller');
 
-router.get('/lab1',labsController.get_lab1);
-router.get('/lab3',labsController.get_lab3);
-router.get('/lab4',labsController.get_lab4);
-router.get('/lab4_preguntas',labsController.get_lab4_preguntas);
-router.get('/lab5',labsController.get_lab5);
-router.get('/lab6',labsController.get_lab6);
-router.get('/lab6_preguntas',labsController.get_lab6_preguntas);
-router.get('/lab11',labsController.get_lab11);
-router.get('/lab12',labsController.get_lab12);
-router.get('/lab13',labsController.get_lab13);
-router.get('/lab13/new',labsController.get_lab13_new);
-router.post('/lab13/new',labsController.post_lab13);
-router.get('/lab13/:id', labsController.get_lab13);
-router.get('/lab17',labsController.get_lab17);
+router.get('/lab1', isAuth, labsController.get_lab1);
+router.get('/lab3',isAuth, labsController.get_lab3);
+router.get('/lab4',isAuth, labsController.get_lab4);
+router.get('/lab4_preguntas',isAuth, labsController.get_lab4_preguntas);
+router.get('/lab5',isAuth, labsController.get_lab5);
+router.get('/lab6',isAuth, labsController.get_lab6);
+router.get('/lab6_preguntas',isAuth, labsController.get_lab6_preguntas);
+router.get('/lab11',isAuth, labsController.get_lab11);
+router.get('/lab12',isAuth, labsController.get_lab12);
+router.get('/lab13',isAuth, labsController.get_lab13);
+router.get('/lab13/new',isAuth ,labsController.get_lab13_new);
+router.post('/lab13/new', isAuth, labsController.post_lab13);
+router.get('/lab13/:id',isAuth, labsController.get_lab13);
+router.get('/lab17',isAuth,labsController.get_lab17);
 
 module.exports = router;
